c0572051a811cec4a567a4a9bdbcaba30cd5e6b2
Cris <cfads2001@gmail.com>
2022-05-25 20:34:22 -0300

    ChannelData component ChannelMessage

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
+import ChannelMessage, {Mention} from "../ChannelMessage";
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
 
 
 const ChannelData: React.FC = () => {
+
+    const messageRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(()=>{
+        const div = messageRef.current;
+
+        if(div)
+            div.scrollTop = div.scrollHeight;
+
+    },[messageRef])
+
     return (
         <Container>
-            <Messages />
+            <Messages ref={messageRef}>
+                <ChannelMessage 
+                    author="Cris"
+                    date="21/06/2021"
+                    content= "Hoje é meu aniversário"
+                />
+                <ChannelMessage 
+                    author="Juan"
+                    date="21/06/2021"
+                    hasMention
+                    content= {
+                        <>
+                            <Mention>@Cris</Mention>, vai ter flex ?
+                        </>
+                    }
+                />
+            </Messages>
 
             <InputWrapper>
                 <Input type="text" placeholder="Conversar em #chat-geral"/>
