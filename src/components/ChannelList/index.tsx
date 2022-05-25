5c6d1aff1f40e570c2c39056065bed3e8dfa335f
Cris <cfads2001@gmail.com>
2022-05-25 06:31:57 -0300

    ChannelButton component

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ChannelButton from "../ChannelButton";
+import { Container, Category, AddCategoryIcon } from "./styles";
+
+
+
+const ChannelList: React.FC = () => {
+    return (
+        <Container>
+          <Category>
+              <span>Canais de texto</span>
+              <AddCategoryIcon />
+          </Category>
+
+          <ChannelButton channelName="chat-geral" />
+          <ChannelButton channelName="chat-músicas" />
+          <ChannelButton channelName="comandos-bots" />
+          <ChannelButton channelName="mural-de-artes" />
+          <ChannelButton channelName="avisos" />
+
+        </Container>
+    )
+};
+
+export default ChannelList;
